75d932a1ec6cab80833f42052c6d88b96730421e
Yash Jain <jain.yash20082004@gmail.com>
2024-08-31 23:06:59 +0530

    some minor changes

--- app/api/getAllBlogs/route.js
+++ app/api/getAllBlogs/route.js
@@ -1,5 +1,3 @@
-// app/api/getAllBlogs/route.js
-
 import clientPromise from '@/lib/mongodb';
 
 export async function GET(req) {
@@ -8,13 +6,17 @@ export async function GET(req) {
     const db = client.db('IdeaSphereBlog');
     const blogsCollection = db.collection('blogs');
 
+    console.log('Fetching blogs from collection:', blogsCollection.collectionName);
+
     // Fetch the blogs
     const blogs = await blogsCollection.find({}).toArray();
-    
+
+   
+
     return new Response(JSON.stringify(blogs), { status: 200 });
   } catch (error) {
-    console.error('Error fetching all blogs:', error);
+    console.error('Error fetching all blogs:', error.message);
+    console.error('Stack trace:', error.stack);
     return new Response('Failed to fetch all blogs', { status: 500 });
   }
 }
-
